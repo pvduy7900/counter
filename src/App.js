272d4474f25e02079d24597d84ca2bbec0a5a8aa
unknown <pvduy7900@gmail.com>
2020-06-09 22:18:05 +0700

    first one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+// import Children from "./components/Children";
 
 function App() {
+  let count = useSelector((state) => state.count); // ???
+  let dispatch = useDispatch();// ???
+  let [color, setColor] = useState("")
+  let textColor = "red"
+  if (count > 3) {
+    textColor = "green"
+  }
+
+  const increaseNum = () => {
+    dispatch({ type: "Increment" }); // dispatch send action, and action is object
+  }; // ????
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Counter App</h1>
+      <h2>{count}</h2>
+
+      <button onClick={() => increaseNum()}>Increment</button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: "Decrement",
+            payload: { num: 1, message: "bitna is amazing" },
+          })
+        }
+      >
+        Decrement
+      </button>
+      <button onClick={() => dispatch({
+        type: "reset",
+      })}> reset </button>
+
+      <input type="text" onChange={(e) => setColor(e.target.value)}></input>
+      <div>
+        <div>
+          {
+            Array(count).fill(null).map(x => {
+              console.log(x)
+              return <div style={{ backgroundColor: color, color: textColor }} className="box">{color}</div>
+            })
+          }
+        </div>
+        
+      </div>
+
     </div>
+
+
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
+const initialState = {
+  count: 0,
+};
+// ???
+function reducer(state = initialState, action) {
+  if (action.type === "Increment") {
+    state.count++;
+  } else if (action.type === "Decrement") {
+    state.count = state.count - action.payload.num;
+    if(state.count < 0){
+      state.count = 0;
+    }
+  } else if (action.type === "reset") {
+    state.count = 0;
+  }
+
+  return { ...state }; // grab everything inside of initialState and make new object and return it
+}
+const store = createStore(reducer);
 ReactDOM.render(
-  <React.StrictMode>
+  // ??? 
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
